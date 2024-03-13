5df866482d315544f6380abb79d6bc9e201011be
Clay Levy <claylevy53@gmail.com>
2024-03-13 03:15:03 +0000

    Completed exercise

---
+++ react-props-and-expressions/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomButton from './CustomButton';
+
+function App() {
+  return (
+    <div>
+      <CustomButton text="I" class="first" />
+      <CustomButton text="Know" class="second" />
+      <CustomButton text="React!" class="third" />
+    </div>
+  );
+}
+
+export default App;
